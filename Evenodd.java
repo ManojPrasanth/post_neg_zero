f59febf15d87a497eb30cd1b57cd52cb97337cfa
ManojPrasanth <manojprasanth.siva@gmail.com>
2016-08-31 21:35:02 +0530

    Add files via upload

---
+++ Evenodd.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Evenodd
+{
+	public static void main(String args[])
+	{
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter the number");
+		int num=s.nextInt();
+			if(num%2==0)
+			{
+			System.out.println("Even");
+			}
+			else
+			System.out.println("Odd");
+	}
+}
